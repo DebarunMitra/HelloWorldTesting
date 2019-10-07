52756e992da6f7ad9bc544c765b4d34fe6025eee
DebarunMitra <debarun96mitra@gmail.com>
2019-10-07 12:58:10 +0530

    JSO version 1.0

---
+++ JobSearchOo/js/script.js
@@ -0,0 +1,13 @@
+// Author: Debarun Mitra
+// Application Name: JobSearchOo
+// Objective: Create a front end application for job search
+class Jobsearch(){
+  constructor(loc,exp,skill,cname){
+    this.loc=loc;
+    this.exp=exp;
+    this.skill=skill;
+    this.cname=cname;
+  }
+
+}
+const js=new Jobsearch();
