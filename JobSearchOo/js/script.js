1950a90480795a5904ba63130e9b6514d429de22
DebarunMitra <debarun96mitra@gmail.com>
2019-10-09 10:06:36 +0530

    JSO version 1.0 (initial stage)

--- JobSearchOo/js/script.js
+++ JobSearchOo/js/script.js
@@ -2,73 +2,101 @@
 // Application Name: JobSearchOo
 // Objective: Create a front end application for job search
 class Jobsearch{
-  constructor(loc,exp,skill,cname){
-    this.loc=loc;
-    this.exp=exp;
-    this.skill=skill;
-    this.cname=cname;
-  }
   getSearchResult(location,skills,experience,cityName){
-    let count=0,regexLoc,regexSkill,regexCname,regexExp=/\d+/g;
+    const monthNo={"Jan":1,"Feb":2,"Mar":3,"Apr":4,"May":5,"Jun":6,"Jul":7,"Aug":8,"Sep":9,"Oct":10,"Nov":11,"Dec":12};
+    let count=0,regexLoc,regexSkill,regexCname,regexExp=/\d+/g,strExp;
     console.log(location+','+skills+','+experience+','+cityName);
     (skills==='')?regexSkill ='.*?':regexSkill = new RegExp("\\b(?:"+skills+")\\b", "gi");
     (location==='')?regexLoc = '.*?':regexLoc = new RegExp("\\b(?:"+location+")\\b", "gi");
     (cityName==='')?regexCname = '.*?':regexCname = new RegExp("\\b(?:"+cityName+")\\b", "gi");
+    (experience==='')?strExp=undefined:strExp=experience;
     console.log(regexLoc+','+regexExp+','+regexSkill+','+regexCname);
-  //   let job=data.filter(ele=>ele.skills.match(regexSkill) && ele.location.match(regexLoc) && ele.companyname.match(regexCname)).map((item)=>{
-  //   if(strExp!==undefined){
-  //     console.log('strExp');
-  //     let expMatch=item.experience.match(regexExp);
-  //     let date=new Date();
-  //     let enddate=item.enddate.split(/,| /);
-  //     if(expMatch!==null){
-  //       if(expMatch[0]!==undefined && parseInt(expMatch[0])<=strExp && parseInt(expMatch[0])!==0 && enddate!==''){
-  //         if(expMatch[1]!==undefined && parseInt(expMatch[1])>=strExp){
-  //             //console.log('expMatch[1]!==undefined && parseInt(expMatch[1])>=strExp');
-  //             console.log(item);
-  //             console.log(expMatch[0]+"-"+expMatch[1]);
-  //             let d=parseInt(enddate[1]);
-  //             console.log(d+"-"+monthNo[enddate[0]]+'-'+enddate[0]);
-  //             count+=1;
-  //         }
-  //         else if(expMatch[1]!==undefined && parseInt(expMatch[1])===strExp){
-  //           //console.log('!!expMatch[1]!==undefined && parseInt(expMatch[1])>=strExp');
-  //           console.log(item);
-  //           console.log(expMatch[0]+"-"+expMatch[1]);
-  //           let d=parseInt(enddate[1]);
-  //           console.log(d+"-"+monthNo[enddate[0]]+'-'+enddate[0]);
-  //           count+=1;
-  //         }
-  //       }
-  //      //  else if(parseInt(expMatch[0])===0){
-  //      //    console.log('parseInt(expMatch[1])===strExp');
-  //      //   console.log(item);
-  //      //   console.log(expMatch[0]+"-"+expMatch[1]);
-  //      //   count+=1;
-  //      // }
-  //     }
-  //    }
-  //   else {
-  //     console.log('Not strExp');
-  //    console.log(item);
-  //     count+=1;
-  //   }
-  // });
-  // console.log('count:'+count);
-  // }).catch(err=>console.log('ERROR:'+err));
-    return new Jobsearch(location,experience,skills,cityName);
+    fetch('https://nut-case.s3.amazonaws.com/jobs.json').then(response=>response.json()).then(item=>{
+    let data=item.data;
+    let job=data.filter(ele=>ele.skills.match(regexSkill) && ele.location.match(regexLoc) && ele.companyname.match(regexCname)).map((item)=>{
+    if(strExp!==undefined){
+      console.log('strExp');
+      let expMatch=item.experience.match(regexExp);
+      let date=new Date();
+      let enddate=item.enddate.split(/,| /);
+      if(expMatch!==null){
+        if(expMatch[0]!==undefined && parseInt(expMatch[0])<=strExp && parseInt(expMatch[0])!==0 && enddate!==''){
+          if(expMatch[1]!==undefined && parseInt(expMatch[1])>=strExp){
+              //console.log('expMatch[1]!==undefined && parseInt(expMatch[1])>=strExp');
+            //  console.log(item);
+              //console.log(expMatch[0]+"-"+expMatch[1]);
+              let d=parseInt(enddate[1]);
+              //console.log(d+"-"+monthNo[enddate[0]]+'-'+enddate[0]);
+              console.log(item.companyname+','+item.title+','+item.jd+','+item.location+','+item.skills+','+item.salary+','+item.enddate+','+item.applylink);
+              count+=1;
+              this.getJobDetails(item.companyname,item.title,item.jd,item.location,item.skills,item.salary,item.enddate,item.applylink);
+          }
+          else if(expMatch[1]!==undefined && parseInt(expMatch[1])===strExp){
+            //console.log('!!expMatch[1]!==undefined && parseInt(expMatch[1])>=strExp');
+          //  console.log(item);
+            //console.log(expMatch[0]+"-"+expMatch[1]);
+            let d=parseInt(enddate[1]);
+            //console.log(d+"-"+monthNo[enddate[0]]+'-'+enddate[0]);
+            console.log(item.companyname+','+item.title+','+item.jd+','+item.location+','+item.skills+','+item.salary+','+item.enddate+','+item.applylink);
+            count+=1;
+            this.getJobDetails(item.companyname,item.title,item.jd,item.location,item.skills,item.salary,item.enddate,item.applylink);
+          }
+        }
+       //  else if(parseInt(expMatch[0])===0){
+       //    console.log('parseInt(expMatch[1])===strExp');
+       //   console.log(item);
+       //   console.log(expMatch[0]+"-"+expMatch[1]);
+       //   count+=1;
+       // }
+      }
+     }
+    else {
+      console.log('Not strExp');
+     console.log(item);
+     console.log(item.companyname+','+item.title+','+item.jd+','+item.location+','+item.skills+','+item.salary+','+item.enddate+','+item.applylink);
+      count+=1;
+      this.getJobDetails(item.companyname,item.title,item.jd,item.location,item.skills,item.salary,item.enddate,item.applylink);
+    }
+  });
+  console.log('count:'+count);
+  }).catch(err=>console.log('ERROR:'+err));
   }
-  getJobDetails(){
-    console.log(this.loc+','+this.exp+','+this.skill+','+this.cname);
+  getJobDetails(jcn,jtitle,jd,jloc,jskill,jsal,japply,jlink){
+    let jobDataTag=document.createElement('p');
+    jobDataTag.innerHTML ='<div class="card" style="width:90%;"><div class="card-body"><h3 class="card-title">'+jcn+'</h3>'+
+    '<p class="card-text">'+jtitle+'</p>'+
+    '</div>'+
+    '<ul class="list-group list-group-flush">'+
+        '<li class="list-group-item">'+
+          '<h6 class="card-title">Job Description:</h6>'+
+          '<p>'+jd+'</p>'+
+        '</li>'+
+          '<li class="list-group-item">'+
+            '<h6 class="card-title">Job Location:</h6>'+
+            '<p>'+jloc+'</p>'+
+          '</li>'+
+          '<li class="list-group-item">'+
+            '<h6 class="card-title">Skill need:</h6>'+
+            '<p>'+jskill+'</p>'+
+          '</li>'+
+          '<li class="list-group-item d-flex">'+
+            '<div class="">'+
+              '<h6 class="card-title">Salary:</h6>'+
+               '<p>'+jsal+'</p>'+
+            '</div>'+
+            '<div class="sal-enddate">'+
+              '<h6 class="card-title">Last Date:</h6>'+
+              '<p>'+japply+'</p>'+
+            '</div>'+
+          '</li>'+
+      '</ul>'+
+      '<div class="card-body">'+
+          '<a href="#" class="'+jlink+'">Apply link</a>'+
+      '</div>'+
+    '</div><br>';
+    document.getElementById("jobContent").appendChild(jobDataTag);
   }
 }
-// Jobsearch.prototype.setJobParam=function(){
-//   let loc=document.getElementById('loc').value;
-//   let exp=document.getElementById('exp').value;
-//   let skill=document.getElementById('skill').value;
-//   let cname=document.getElementById('cname').value;
-//   return new Jobsearch(loc,exp,skill,cname);
-// }
 const js=new Jobsearch();
 let search=document.getElementById('search');
   let loc=document.getElementById('loc');
@@ -77,6 +105,6 @@ let search=document.getElementById('search');
   let cname=document.getElementById('cname');
 search.addEventListener('click',function(){
   js.getSearchResult(loc.value,skill.value,exp.value,cname.value);
-  const jso=new Jobsearch();
-  jso.getJobDetails();
+//  const jso=new Jobsearch();
+  //jso.getJobDetails();
 },false);
