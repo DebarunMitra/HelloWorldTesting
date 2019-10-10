a7ae9f2fe654b77682387ab5c1bb9d7c21afe310
DebarunMitra <debarun96mitra@gmail.com>
2019-10-10 15:07:39 +0530

    randomNumber working

--- QuizOnApp/routesJS/routes.js
+++ QuizOnApp/routesJS/routes.js
@@ -1,13 +1,38 @@
 class Question
 {
-  constructor(q,a,b,c,d,qset,qid){
-    this.q=q;
-    this.a=a;
-    this.b=b;
-    this.c=c;
-    this.d=d;
-    this.qset=qset;
-    this.qid=qid;
+  constructor(count){
+    this.count=count;
+    this.qno=[];
+    this.questions=[];
+  }
+  randomNumber(question,length){
+    let no=Math.floor(Math.random() * (length-1));
+    if(!this.qno.includes(no) && no>0)
+    {
+      this.qno.push(no);
+        //console.log("ranDom: "+no);
+        let q=question.filter(item => item.qid===no).map((qset) => {
+        //this.questions.push(qset.question)
+      //  console.log({"q":qset.question,"a":qset[1],"b":qset[2],"c":qset[3],"d":qset[4],"qset":'per',"qid":qset.qid});
+        this.questions[this.count-1]={"q":qset.question,"a":qset[1],"b":qset[2],"c":qset[3],"d":qset[4],"qset":'per',"qid":qset.qid};
+      });
+        this.count-=1;
+        if(this.count===0){
+          console.log(this.questions);
+          return
+        }
+         if(this.count>0){
+           //console.log("count: "+this.count);
+           this.randomNumber(question,length);
+         }
+      //return no;
+    }
+    else {
+      //console.log("ranDom repeat");
+      //console.log("repeat: "+no);
+      //return this.randomNumber(number-1);
+      this.randomNumber(question,length);
+    }
   }
 }
 module.exports = (app, db) => {
@@ -20,8 +45,16 @@ app.get('/ranQue',(req,res)=>{
               db.find({"q_set":"per"},{projection:{"_id":0,"questions":1}},(err, result) => {
                    (err===true)?console.log(err + " this error has occured"):(dbVal=result.toArray());
               });
-            dbVal.then(que=>{
-              console.log(que[0].questions);
+              dbVal.then(que=>{
+              let question=que[0].questions;
+              const qno=new Question(noq);
+              let getQandO=[],len=que[0].questions.length,no=0;
+              let num=qno.randomNumber(question,len);
+              // question.slice(0,10).filter(item=>{
+              //   let num=qno.randomNumber(len);
+              //   console.log("num: "+num);
+              //   return (item.qid===num);
+              // }).map(qset => {console.log(qset);});
             });
           });
 
