2c7fab6949caf5ba45cb10ddf9b883c48426e8d1
DebarunMitra <debarun96mitra@gmail.com>
2019-10-20 10:21:54 +0530

    error msg with bootstrap alert

--- nodeJS-MERN/QuizONwithNodeJsAUTH/routes/users.js
+++ nodeJS-MERN/QuizONwithNodeJsAUTH/routes/users.js
@@ -9,8 +9,32 @@ router.get('/register',(req,res)=>res.render('register'));
 
 //register handle
 router.post('/register',(req,res)=>{
-  console.log(req.body);
-  res.send('Data Send!');
+  //console.log(req.body);
+  //res.send('Data Send!');
+  const {name,email,password,password2}=req.body;
+  let errors=[];
+
+  //check required fields
+  if(!name || !email || !password || !password2){
+    errors.push({msg:'Please enter all fiends'});
+  }
+  //password check
+  if(password!==password2){
+    errors.push({msg:'Passwords are not match'});
+  }
+  //check pass length
+  if(password.length<6){
+    errors.push({msg:'Passwords should be  at least  6 charecters'});
+  }
+
+  if(errors.length>0){
+    res.render('register',{
+      errors,name,email,password,password2
+    });
+  }
+  else{
+    res.send('pass');
+  }
 });
 
 module.exports=router;
