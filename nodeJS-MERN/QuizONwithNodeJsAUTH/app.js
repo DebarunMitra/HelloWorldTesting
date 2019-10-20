2e1af63a08bc6da21f32b796601ba51389653a94
DebarunMitra <debarun96mitra@gmail.com>
2019-10-20 17:14:38 +0530

    passport js configuration, moddleware add

--- nodeJS-MERN/QuizONwithNodeJsAUTH/app.js
+++ nodeJS-MERN/QuizONwithNodeJsAUTH/app.js
@@ -3,8 +3,12 @@ const expressLayouts=require('express-ejs-layouts');
 const mongoose=require('mongoose');
 const flash=require('connect-flash');
 const session=require('express-session');
+const passport=require('passport');
 
 const app=express();
+
+//passport config
+require('./config/passport')(passport);
 
 //db config
 const db=require('./config/keys').MongoURI;
@@ -28,6 +32,11 @@ app.use(
     saveUninitialized: true
   })
 );
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 //connect flash
 app.use(flash());
