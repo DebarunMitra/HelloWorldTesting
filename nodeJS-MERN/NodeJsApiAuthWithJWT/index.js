69e613e278039f93087466d17a30ac551b6ae31a
DebarunMitra <debarun96mitra@gmail.com>
2019-10-21 09:45:07 +0530

    nodejs authentication and authorisation

---
+++ nodeJS-MERN/NodeJsApiAuthWithJWT/index.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const app=express();
+
+const PORT=5020;
+app.listen(PORT,()=>console.log('Server up and running...');)
---
+++ nodeJS-MERN/QuizONwithNodeJsAUTH/config/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+  MongoURI:"mongodb+srv://root:root@cluster0-7a3h9.mongodb.net/test?retryWrites=true&w=majority"
+}
