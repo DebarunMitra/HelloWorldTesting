ad9ecf99c26f1c4746ad0d43715589d23b23d3c9
DebarunMitra <debarun96mitra@gmail.com>
2019-10-21 23:12:31 +0530

    json web token added

--- nodeJS-MERN/NodeJsApiAuthWithJWT/routes/auth.js
+++ nodeJS-MERN/NodeJsApiAuthWithJWT/routes/auth.js
@@ -2,6 +2,7 @@ const router=require('express').Router();
 const User=require('../models/User');
 const {registerValidation,loginValidation}=require('../validation');
 const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
 
 
 router.post('/register', async (req,res)=>{//async wait until the data save into the db.
