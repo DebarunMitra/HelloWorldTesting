b7ac1c1e2865e38772a930fd9ba84edcf4020280
DebarunMitra <debarun96mitra@gmail.com>
2019-10-23 15:29:35 +0530

    path, express-handlebars package added

--- StoryYO/app.js
+++ StoryYO/app.js
@@ -1,56 +1,69 @@
-const express=require('express');
-const mongoose=require('mongoose');
-const passport=require('passport');
-const cookieParser=require('cookie-parser');
-const session=require('express-session');
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
 
-//load model
+// Load User Model
 require('./models/User');
 
-//passport config
+// Passport Config
 require('./config/passport')(passport);
 
-//load routes
-const auth=require('./routes/auth');
+// Load Routes
+const index = require('./routes/index');
+const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
-//load keys
-const keys=require('./config/keys');
+// Load Keys
+const keys = require('./config/keys');
 
-//map global promises
-mongoose.Promise=global.Promise;
+// Map global promises
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+mongoose.connect(keys.mongoURI, {
+  useMongoClient:true
+})
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-//mongoose connection
-mongoose.connect(keys.mongoURI,{useNewUrlParser: true,useUnifiedTopology: true}).then(()=>console.log('MongoDB connected...')).catch((err) =>console.log(err));
+const app = express();
 
-
-const app=express();
-
-app.get('/',(req,res)=>{
-  res.send('Its Working');
-});
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine', 'handlebars');
 
 app.use(cookieParser());
 app.use(session({
-  secret:'secret',
-  resave:false,
-  saveUninitialized:false
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
 }));
 
-//passport middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-//set global vars
-app.use((req,res,next)=>{
-  res.locals.user=req.user || null;
+// Set global vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null;
   next();
-})
+});
 
-//user routes
-app.use('/auth',auth);
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-const port=process.env.PORT || 5020;
+// Use Routes
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/stories', stories);
 
-app.listen(port,()=>{
-  console.log(`Server started on port ${port}`);
-})
+const port = process.env.PORT || 5020;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+});
