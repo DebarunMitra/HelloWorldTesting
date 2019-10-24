4c63bd6bcd2c9852b5d712d1b9b5ae418d6fe4ae
DebarunMitra <debarun96mitra@gmail.com>
2019-10-24 12:50:42 +0530

    helpers hbs add

--- StoryYO/app.js
+++ StoryYO/app.js
@@ -22,6 +22,11 @@ const stories = require('./routes/stories');
 // Load Keys
 const keys = require('./config/keys');
 
+//helper handlebars
+const {
+  truncate,stripTags
+}=require('./helpers/hbs')
+
 // Map global promises
 mongoose.Promise = global.Promise;
 // Mongoose Connect
@@ -37,6 +42,10 @@ app.use(bodyParser.json());
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers:{
+    truncate:truncate,
+    stripTags:stripTags
+  },
   defaultLayout:'main'
 }));
 app.set('view engine', 'handlebars');
