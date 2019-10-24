4d34b4448304e9d8fd50adb46484421cd058125a
DebarunMitra <debarun96mitra@gmail.com>
2019-10-24 11:03:57 +0530

    story save to db

--- StoryYO/routes/stories.js
+++ StoryYO/routes/stories.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose=require('mongoose');
+const User=mongoose.model('users');
+const Story=mongoose.model('stories');
 const {ensureAuthenticated, ensureGuest} = require('../helpers/auth');
 
 // Stories Index
@@ -14,7 +17,26 @@ router.get('/add', ensureAuthenticated, (req, res) => {
 
 //process add Story
 router.post('/',(req,res)=>{
-  res.send('sent');
+  //console.log(req.body);
+  let allowComments;
+  if(req.body.allowComments){
+    allowComments=true;
+  }else{
+    allowComments=false;
+  }
+  //story object created
+  const newStory={
+    title:req.body.title,
+    body:req.body.body,
+    status:req.body.status,
+    allowComments:allowComments,
+    user:req.user.id
+  };
+  //save story object into the db
+    new Story(newStory).save().then((story) => {
+      res.redirect(`/stories/show/${story.id}`);
+      console.log(story.id);
+    }).catch((err) =>console.log('Story Error:'+err));
 });
 
 module.exports = router;
