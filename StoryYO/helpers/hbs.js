6c06b48fe6187d20b983e611fff0a91d97052e3e
DebarunMitra <debarun96mitra@gmail.com>
2019-10-28 23:47:26 +0530

    helper created

--- StoryYO/helpers/hbs.js
+++ StoryYO/helpers/hbs.js
@@ -2,7 +2,7 @@ const moment=require('moment');
 module.exports = {
   truncate: function(str, len){
     if (str.length > len && str.length > 0) {
-			var new_str = str + " ";
+			let new_str = str + " ";
 			new_str = str.substr(0, len);
 			new_str = str.substr(0, new_str.lastIndexOf(" "));
 			new_str = (new_str.length > 0) ? new_str : str.substr(0, len);
@@ -18,5 +18,38 @@ module.exports = {
   },
   select: function(selected, options){
     return options.fn(this).replace( new RegExp(' value=\"' + selected + '\"'), '$& selected="selected"').replace( new RegExp('>' + selected + '</option>'), ' selected="selected"$&');
+  },
+  senNo:function(sen){
+    let sc=sen.split(':');
+    return sc[0];
+  },
+  wordNo:function(word){
+    let wc=word.split(':');
+    return wc[1];
+  },
+  paragraphNo:function(para){
+    let pc=para.split(':');
+    return pc[2];
+  },
+  eachProperty: function(value,options) {
+    let context=JSON.parse(value);
+    var ret = "";
+    //console.log(typeof(context));
+    //console.log(Object.keys(context).length);
+    for(let prop in context)
+    {
+        ret = ret + options.fn({property:prop,value:context[prop]});
+    }
+    return ret;
+  },
+  readingTime:function(data){
+    let words=data.split(':');
+    let time=(words[1]/200)*60;
+    return time.toFixed(2);
+  },
+  speakingTime:function(data){
+    let words=data.split(':');
+    let time=(words[1]/130)*60;
+    return time.toFixed(2);
   }
 }
