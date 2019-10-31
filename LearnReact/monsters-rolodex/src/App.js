c496c1dc6ae9863ca920b36ea85eb604b88b9422
DebarunMitra <debarun96mitra@gmail.com>
2019-10-31 17:37:12 +0530

    search input added

--- LearnReact/monsters-rolodex/src/App.js
+++ LearnReact/monsters-rolodex/src/App.js
@@ -18,6 +18,7 @@ class App extends Component{
   render(){
     return (
       <div className="App">
+      <input type="search" placeholder="Search Monsters"/>
       <CardList monsters={this.state.monsters} /></div>
     );
   }
