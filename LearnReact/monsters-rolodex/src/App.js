fd5828c5c014f38a6014fc7fcc71faa28d5464f8
DebarunMitra <debarun96mitra@gmail.com>
2019-10-31 19:09:07 +0530

    title added

--- LearnReact/monsters-rolodex/src/App.js
+++ LearnReact/monsters-rolodex/src/App.js
@@ -18,13 +18,19 @@ class App extends Component{
     fetch('https://jsonplaceholder.typicode.com/users').then((response) => response.json()).then((users) =>
     this.setState({monsters:users}));
   }
+
+handleChange=(e)=>{
+  this.setState({searchField:e.target.value});
+};
+
   render(){
     const {monsters,searchField}=this.state;
     const filteredMonsters=monsters.filter((monster) =>monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
     return (
       <div className="App">
-      <SearchBox placeholder='Search Monster' handleChange={e=>{this.setState({searchField:e.target.value})}} />
+      <h1>Monster Rolodex</h1>
+      <SearchBox placeholder='Search Monster' handleChange={this.handleChange} />
       {/*<CardList monsters={this.state.monsters} />*/}
       <CardList monsters={filteredMonsters} />
       </div>
