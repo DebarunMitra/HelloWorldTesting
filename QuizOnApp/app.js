fc3a59809f227cf7e3ed5ff91152cab7eb8c781a
DebarunMitra <debarun96mitra@gmail.com>
2019-11-05 14:45:47 +0530

    mongoose configuration

--- QuizOnApp/app.js
+++ QuizOnApp/app.js
@@ -1,9 +1,14 @@
 const express=require('express');
-const mongodb=require('mongodb').MongoClient;
+//const mongodb=require('mongodb').MongoClient;
+const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const JSON = require('circular-json');
 const path=require('path');//to access public directory
 const cors=require('cors');
+
+// Load User Model
+require('./models/Questions');
+require('./models/User');
 
 const router = express.Router();
 
@@ -23,13 +28,40 @@ app.use(bodyParser.json());//initializing bodyParser middleware
 
 const db=require('./config/db');
 
+
+
 const port=5020;
 
-mongodb.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+// mongodb.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+//     if (err)
+//     return console.log(err);
+//     const database =db.db("quizOn").collection("questions");
+//     require('./routesJS/routes')(app, database);
+//     app.listen(port, () => {
+//         console.log(`Server up on port no  ${port}`);
+//         console.log('Database Connected Successfully');
+//     });
+// });
+// mongoose.connect(db, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
+//
+//
+//   app.listen(port, () => {
+//     console.log(`Server started on port ${port}`)
+//   });
+
+// Use Routes
+//app.use('/', routes);
+
+mongoose.connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
     if (err)
     return console.log(err);
-    const database =db.db("quizOn").collection("questions");
-    require('./routesJS/routes')(app, database);
+    //const database =db.collection("questions");
+    require('./routesJS/routes')(app);
     app.listen(port, () => {
         console.log(`Server up on port no  ${port}`);
         console.log('Database Connected Successfully');
