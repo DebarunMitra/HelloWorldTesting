267b75cc4974ea67d188af57c609c2d71887514d
DebarunMitra <debarun96mitra@gmail.com>
2019-11-10 20:34:40 +0530

    configuration

--- StoryYO/config/keys.js
+++ StoryYO/config/keys.js
@@ -1,5 +1,5 @@
-module.exports={
-  mongoURI:'mongodb+srv://storyyo:storyyo@cluster0-ozxed.mongodb.net/test?retryWrites=true&w=majority',
-  googleClientID:'1093526891395-76q58c0n661htiqaprcmeq9sagvvn3q1.apps.googleusercontent.com',
-  googleClientSecret:'SoenQIYk-j4S1I5OZjrPk5DK'
+if(process.env.NODE_ENv==='production'){
+  module.exports=require('./keys_prod');
+}else{
+  module.exports=require('./keys_dev');
 }
